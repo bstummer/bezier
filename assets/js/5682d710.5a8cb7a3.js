d0998ae92ab44aa14b1e7d15cfb83ccb469671e8
Vaschex <52933850+bstummer@users.noreply.github.com>
2022-11-04 18:16:13 +0100

    Built and published by Moonwave

---
+++ assets/js/5682d710.5a8cb7a3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[526],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/bstummer/bezier/blob/main","baseUrl":"/bezier/","classOrder":[],"apiCategories":[]}')}}]);
